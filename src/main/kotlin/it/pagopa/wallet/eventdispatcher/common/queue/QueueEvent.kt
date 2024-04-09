13d4ed1c0915cf263e230c2e3ab39a2c395e1b0b
petretiandrea <petretiandrea@gmail.com>
2024-04-09 18:06:25 +0200

    add traceinfo

--- src/main/kotlin/it/pagopa/wallet/eventdispatcher/common/queue/QueueEvent.kt
+++ src/main/kotlin/it/pagopa/wallet/eventdispatcher/common/queue/QueueEvent.kt
@@ -2,5 +2,4 @@ package it.pagopa.wallet.eventdispatcher.common.queue
 
 import it.pagopa.wallet.eventdispatcher.domain.WalletEvent
 
-// TODO: add trace info
-data class QueueEvent<T : WalletEvent>(val data: T)
+data class QueueEvent<T : WalletEvent>(val data: T, val tracingInfo: TracingInfo? = null)
---
+++ src/main/kotlin/it/pagopa/wallet/eventdispatcher/common/queue/TracingInfo.kt
@@ -0,0 +1,7 @@
+package it.pagopa.wallet.eventdispatcher.common.queue
+
+data class TracingInfo(
+    val traceparent: String,
+    val tracestate: String?,
+    val baggage: String?
+)
