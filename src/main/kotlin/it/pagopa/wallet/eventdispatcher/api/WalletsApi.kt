dbce33d49ac43f7bede9f43da4cb42babae6f64f
petretiandrea <petretiandrea@gmail.com>
2024-04-12 15:47:27 +0200

    Update src/main/kotlin/it/pagopa/wallet/eventdispatcher/api/WalletsApi.kt
    Co-authored-by: Pietro Tota <115724836+pietro-tota@users.noreply.github.com>

--- src/main/kotlin/it/pagopa/wallet/eventdispatcher/api/WalletsApi.kt
+++ src/main/kotlin/it/pagopa/wallet/eventdispatcher/api/WalletsApi.kt
@@ -22,5 +22,15 @@ class WalletsApi(private val walletsApiClient: it.pagopa.generated.wallets.api.W
                 UpdateWalletUsageRequest().clientId(clientId).usageTime(usedAt)
             )
             .map { ResponseEntity(Unit, it.headers, it.statusCode) }
+            .doOnError {
+                if (it is WebClientResponseException) {
+                    logger.error(
+                        "Error updating last usage data. Received HTTP error code: [${it.statusCode}], response body: [${it.responseBodyAsString}]",
+                        it
+                    )
+                } else {
+                    logger.error("Error updating last usage data.", it)
+                }
+            }
     }
 }
