aecfb565156a669153b2d8a8095eb51adaf7ad9f
Pietro Tota <pietro.tota@pagopa.it>
2024-06-13 10:27:44 +0200

    fix: refactor wallet created event type discriminator

--- src/main/kotlin/it/pagopa/wallet/eventdispatcher/common/serialization/WalletEventMixin.kt
+++ src/main/kotlin/it/pagopa/wallet/eventdispatcher/common/serialization/WalletEventMixin.kt
@@ -2,7 +2,7 @@ package it.pagopa.wallet.eventdispatcher.common.serialization
 
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
-import it.pagopa.wallet.eventdispatcher.common.serialization.WalletEventMixin.Companion.WALLET_EXPIRED_TYPE
+import it.pagopa.wallet.eventdispatcher.common.serialization.WalletEventMixin.Companion.WALLET_CREATED_TYPE
 import it.pagopa.wallet.eventdispatcher.common.serialization.WalletEventMixin.Companion.WALLET_USED_TYPE
 import it.pagopa.wallet.eventdispatcher.domain.WalletCreatedEvent
 import it.pagopa.wallet.eventdispatcher.domain.WalletUsedEvent
@@ -10,11 +10,11 @@ import it.pagopa.wallet.eventdispatcher.domain.WalletUsedEvent
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
 @JsonSubTypes(
     JsonSubTypes.Type(value = WalletUsedEvent::class, name = WALLET_USED_TYPE),
-    JsonSubTypes.Type(value = WalletCreatedEvent::class, name = WALLET_EXPIRED_TYPE)
+    JsonSubTypes.Type(value = WalletCreatedEvent::class, name = WALLET_CREATED_TYPE)
 )
 class WalletEventMixin {
     companion object {
         const val WALLET_USED_TYPE = "WalletUsed"
-        const val WALLET_EXPIRED_TYPE = "WalletCreated"
+        const val WALLET_CREATED_TYPE = "WalletCreated"
     }
 }
