3dfceb07920030f93b4e171fa32744b9a78ae165
e.serrao <emanuele.serrao@bvtech.com>
2024-10-16 15:56:47 +0200

    updated usage of mono

--- src/main/kotlin/it/pagopa/wallet/eventdispatcher/service/WalletCDCService.kt
+++ src/main/kotlin/it/pagopa/wallet/eventdispatcher/service/WalletCDCService.kt
@@ -3,13 +3,13 @@ package it.pagopa.wallet.eventdispatcher.service
 import it.pagopa.wallet.eventdispatcher.audit.LoggingEvent
 import java.util.*
 import org.slf4j.LoggerFactory
-import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
 
 @Component
 class WalletCDCService(
-    private val cdcKafkaTemplate: KafkaTemplate<String, Any>,
+    private val cdcKafkaTemplate: ReactiveKafkaProducerTemplate<String, Any>,
     private val cdcTopicName: String
 ) {
 
@@ -18,7 +18,8 @@ class WalletCDCService(
     fun sendToKafka(event: LoggingEvent): Mono<Unit> {
         log.info("Sending CDC event to Kafka: [{}]", event.id)
 
-        return Mono.fromCallable { cdcKafkaTemplate.send(cdcTopicName, event.id, event) }
+        return cdcKafkaTemplate
+            .send(cdcTopicName, event.id, event)
             .doOnSuccess { log.info("Successfully sent CDC event to Kafka: [{}]", event.id) }
             .doOnError { log.error("Failed to send CDC event to Kafka: [{}]", event.id, it) }
             .thenReturn(Unit)
