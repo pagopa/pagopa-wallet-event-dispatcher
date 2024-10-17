22845031b23628154ddd874c6ff632d269ea3cc7
e.serrao <emanuele.serrao@bvtech.com>
2024-10-17 18:01:10 +0200

    fixed parent constructor

--- src/main/kotlin/it/pagopa/wallet/eventdispatcher/audit/LoggingEvent.kt
+++ src/main/kotlin/it/pagopa/wallet/eventdispatcher/audit/LoggingEvent.kt
@@ -5,16 +5,13 @@ import java.util.*
 
 sealed class LoggingEvent(val id: String, val timestamp: String) {
     constructor() : this(UUID.randomUUID().toString(), Instant.now().toString())
+    constructor(id: String) : this(id, Instant.now().toString())
 }
 
-data class WalletAddedEvent(val walletId: String) :
-    LoggingEvent(walletId, Instant.now().toString())
+data class WalletAddedEvent(val walletId: String) : LoggingEvent(walletId)
 
-data class WalletMigratedAddedEvent(val walletId: String) :
-    LoggingEvent(walletId, Instant.now().toString())
+data class WalletMigratedAddedEvent(val walletId: String) : LoggingEvent(walletId)
 
-data class WalletDeletedEvent(val walletId: String) :
-    LoggingEvent(walletId, Instant.now().toString())
+data class WalletDeletedEvent(val walletId: String) : LoggingEvent(walletId)
 
-data class WalletDetailsAddedEvent(val walletId: String) :
-    LoggingEvent(walletId, Instant.now().toString())
+data class WalletDetailsAddedEvent(val walletId: String) : LoggingEvent(walletId)
