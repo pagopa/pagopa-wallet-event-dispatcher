abd7cbccb550f033e09dd52650f2c803ef99d25c
e.serrao <emanuele.serrao@bvtech.com>
2024-10-17 17:58:50 +0200

    fixed walletId as id

--- src/main/kotlin/it/pagopa/wallet/eventdispatcher/audit/LoggingEvent.kt
+++ src/main/kotlin/it/pagopa/wallet/eventdispatcher/audit/LoggingEvent.kt
@@ -7,12 +7,14 @@ sealed class LoggingEvent(val id: String, val timestamp: String) {
     constructor() : this(UUID.randomUUID().toString(), Instant.now().toString())
 }
 
-data class WalletAddedEvent(val walletId: String) : LoggingEvent()
+data class WalletAddedEvent(val walletId: String) :
+    LoggingEvent(walletId, Instant.now().toString())
 
-data class WalletMigratedAddedEvent(val walletId: String) : LoggingEvent()
+data class WalletMigratedAddedEvent(val walletId: String) :
+    LoggingEvent(walletId, Instant.now().toString())
 
-data class WalletDeletedEvent(val walletId: String) : LoggingEvent()
+data class WalletDeletedEvent(val walletId: String) :
+    LoggingEvent(walletId, Instant.now().toString())
 
-data class WalletDetailsAddedEvent(val walletId: String) : LoggingEvent()
-
-data class ApplicationCreatedEvent(val serviceId: String) : LoggingEvent()
+data class WalletDetailsAddedEvent(val walletId: String) :
+    LoggingEvent(walletId, Instant.now().toString())
