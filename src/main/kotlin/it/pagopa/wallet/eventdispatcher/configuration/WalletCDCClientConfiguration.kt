fcb5569cdc6266c23469c6db7c496d904b121338
EmanueleBVtech <emanuele.serrao@bvtech.com>
2024-10-25 08:59:52 +0200

    Feat [CHK-3333] Added integration tests (#37)
    * init commit
    
    * fixed unit tests
    
    * spotless apply
    
    * fixed tests
    
    * fixed log
    
    * update
    
    * removed redundant log
    
    * fixed .env
    
    * restored open telemetry
    
    * finalized
    
    * fixed mono defer
    
    * updated configuration
    
    * added error log
    
    * finalized
    
    * added integration test
    
    * fixed target pipeline
    
    * added env file
    
    * updated .env local
    
    * updated docker compose
    
    * updated event
    
    * adding kafka
    
    * verify kafka istalled
    
    * fix
    
    * added sh
    
    * union of 2 steps
    
    * fixed port
    
    * multiple tests at once
    
    * attempt reaching kafka
    
    * added meta inf
    
    * disabled sasl on local docker testing
    
    * finalized
    
    * readded event
    
    * target event now in in a file
    
    * updated files
    
    * Update application.properties
    
    * removed conn string
    
    * removed conn string
    
    * added local env
    
    * Update docker-compose.yml
    
    * moved variable to env file
    
    * fixed properties

--- src/main/kotlin/it/pagopa/wallet/eventdispatcher/configuration/WalletCDCClientConfiguration.kt
+++ src/main/kotlin/it/pagopa/wallet/eventdispatcher/configuration/WalletCDCClientConfiguration.kt
@@ -7,31 +7,35 @@ import org.apache.kafka.common.config.SaslConfigs
 import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer
 import org.springframework.kafka.support.serializer.JsonSerializer
 import reactor.kafka.sender.SenderOptions
 
 @Configuration
-class WalletCDCClientConfiguration {
+class WalletCDCClientConfiguration(private val environment: Environment) {
 
     @Bean(name = ["cdcEventHubClient"])
     fun cdcKafkaTemplate(
         walletCDCConfiguration: WalletCDCConfiguration
     ): ReactiveKafkaProducerTemplate<String, Any> {
         val configProps =
-            mapOf(
+            mutableMapOf<String, Any>(
                 ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to walletCDCConfiguration.bootstrapServers,
                 ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
                 ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to JsonSerializer::class.java,
                 ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS to
-                    ErrorHandlingDeserializer::class.java,
-                SaslConfigs.SASL_JAAS_CONFIG to
-                    "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"\$ConnectionString\" password=\"${walletCDCConfiguration.connectionString}\";",
-                SaslConfigs.SASL_MECHANISM to "PLAIN",
-                CommonClientConfigs.SECURITY_PROTOCOL_CONFIG to "SASL_SSL"
+                    ErrorHandlingDeserializer::class.java
             )
 
+        // Local testing in Docker do not require SASL
+        if (!environment.activeProfiles.contains("local")) {
+            configProps[SaslConfigs.SASL_JAAS_CONFIG] =
+                "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"\$ConnectionString\" password=\"${walletCDCConfiguration.connectionString}\";"
+            configProps[SaslConfigs.SASL_MECHANISM] = "PLAIN"
+            configProps[CommonClientConfigs.SECURITY_PROTOCOL_CONFIG] = "SASL_SSL"
+        }
         val senderOptions = SenderOptions.create<String, Any>(configProps)
         return ReactiveKafkaProducerTemplate(senderOptions)
     }
